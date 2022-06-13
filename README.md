c9cfc1aa1d3cd83f6adb337410cfe93a771355e6
Wállaci Aparecido <wallaci.contato@gmail.com>
2022-06-13 07:36:16 -0300

    Adição dos cards.

---
+++ README.md
@@ -0,0 +1,5 @@
+## Projeto para estudo - React + Tailwindcss.
+
+## ☕ Clique no link abaixo para testar a demo.
+
+[DEMO](https://github.com/codewallaci)
---
+++ src/components/Cards.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+
+import Single from "../assets/single.png";
+import Double from "../assets/double.png";
+import Triple from "../assets/triple.png";
+
+export const Cards = () => {
+  return (
+    <div className="w-full py-[10rem] px-4 bg-white">
+      <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8">
+        <div className="w-full shadow-xl hover:shadow-blue-500 flex flex-col p-4 m-4 rounded-lg hover:scale-105 duration-300 border border-blue-900 hover:border-blue-300">
+          <img
+            className="w-20 mx-auto mt-1 bg-transparent"
+            src={Single}
+            alt=""
+          />
+          <h2 className="text-2xl font-bold text-center py-8">Único usuário</h2>
+          <p className="text-center text-4xl font-bold">R$99,99</p>
+          <div className="text-center font-medium">
+            <p className="py-2 border-b mx-8 mt-8">500 GB de armazenamento</p>
+            <p className="py-2 border-b mx-8 ">1 usuário</p>
+            <p className="py-2 border-b mx-8 ">Velocidade de 2 GB</p>
+          </div>
+          <button
+            className="w-[200px] my-6 mx-auto py-2 bg-[#00b8ff]
+             text-white font-medium rounded-md"
+          >
+            Iniciar avaliação
+          </button>
+        </div>
+        <div className="w-full shadow-xl hover:shadow-blue-500 flex flex-col p-4 m-4 rounded-lg hover:scale-105 duration-300 border border-blue-900 hover:border-blue-300">
+          <img
+            className="w-20 mx-auto mt-1 bg-transparent"
+            src={Double}
+            alt=""
+          />
+          <h2 className="text-2xl font-bold text-center py-8">Dois usuário</h2>
+          <p className="text-center text-4xl font-bold">R$150,00</p>
+          <div className="text-center font-medium">
+            <p className="py-2 border-b mx-8 mt-8">800 GB de armazenamento</p>
+            <p className="py-2 border-b mx-8 ">2 usuário</p>
+            <p className="py-2 border-b mx-8 ">Velocidade de 5 GB</p>
+          </div>
+          <button
+            className="w-[200px] my-6 mx-auto py-2 bg-[#009bd6]
+             text-white font-medium rounded-md"
+          >
+            Iniciar avaliação
+          </button>
+        </div>
+        <div className="w-full shadow-xl hover:shadow-blue-500 flex flex-col p-4 m-4 rounded-lg hover:scale-105 duration-300 border border-blue-900 hover:border-blue-300">
+          <img
+            className="w-20 mx-auto mt-1 bg-transparent"
+            src={Triple}
+            alt=""
+          />
+          <h2 className="text-2xl font-bold text-center py-8">Três usuário</h2>
+          <p className="text-center text-4xl font-bold">R$199,99</p>
+          <div className="text-center font-medium">
+            <p className="py-2 border-b mx-8 mt-8">1000 GB de armazenamento</p>
+            <p className="py-2 border-b mx-8 ">3 usuário</p>
+            <p className="py-2 border-b mx-8 ">Velocidade de 10 GB</p>
+          </div>
+          <button
+            className="w-[200px] my-6 mx-auto py-2 bg-[#00b8ff]
+             text-white font-medium rounded-md"
+          >
+            Iniciar avaliação
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
